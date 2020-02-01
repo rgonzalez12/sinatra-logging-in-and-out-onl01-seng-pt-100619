166c5c55aa2686eef42e23487523f80b0a2e7d31
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2020-02-01 20:21:23 +0000

    Automatically backed up by Learn

--- db/migrate/20200201201316_users.rb
+++ db/migrate/20200201201316_users.rb
@@ -1,6 +1,6 @@
 class Users < ActiveRecord::Migration[5.1]
   def change
-    create_table do |t|
+    create_table :users do |t|
       t.string :username
       t.string :password
       t.decimal :balance
